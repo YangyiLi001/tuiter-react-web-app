4260d98dacff7ff7a27f59dc6ad21f2d398d8a65
YangyiLi001 <yangyili1994@gmail.com>
2022-10-23 17:19:18 -0700

    a6 React.js sp22

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -13,6 +13,7 @@ function Tuiter() {
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
                 <Routes>
+                    <Route index element={<ExploreComponent/>}/>
                     <Route path="home"    element={<HomeComponent/>}/>
                     <Route path="explorescreen" element={<ExploreComponent/>}/>
                 </Routes>
--- src/tuiter/post-list/post-item.js
+++ src/tuiter/post-list/post-item.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 const PostItem = (
     {post}
 ) => {
@@ -6,12 +7,18 @@ const PostItem = (
         <li className="list-group-item">
             <div className="row">
                 <div className="col-2">
-                    <img className="rounded-circle" height={50} src={post.avatarIcon}/>
+                    <img className="rounded-circle" height={48} src={post.avatarIcon}/>
                 </div>
                 <div className="col-10">
                     <div><span className="fw-bolder">{post.userName}</span> @{post.handle}. {post.time}</div>
                     <div>{post.content}</div>
-                    <div><img className="rounded-3" src={post.image}/></div>
+                    <div><img width="100%" className="rounded-3" src={post.image}/></div>
+                    <div style={{display:"flex", justifyContent: 'space-evenly'}}>
+                        <span><i className="bi bi-chat"></i> {post.commentNumber}</span>
+                        <span><i className="bi bi-repeat"></i> {post.reTweetNumber}</span>
+                        <span><i className="bi bi-heart"></i> {post.heartNumber}</span>
+                        <span><i className="bi bi-download"></i></span>
+                    </div>
 
                 </div>
             </div>
